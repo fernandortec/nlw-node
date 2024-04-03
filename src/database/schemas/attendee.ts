af8a75e3c31b783517743124506aa12e608d4a79
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2024-04-03 12:16:20 -0300

    :sparkles: (schemas) add atenddees

---
+++ src/database/schemas/attendee.ts
@@ -0,0 +1,24 @@
+import { events } from "@/database/schemas";
+import { type InferSelectModel, relations } from "drizzle-orm";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const attendees = sqliteTable("attendees", {
+	id: integer("id").notNull().primaryKey({ autoIncrement: true }),
+	name: text("name").notNull(),
+	email: text("string").notNull(),
+	createdAt: integer("created_at", { mode: "timestamp" }),
+
+	eventId: text("event_id")
+		.notNull()
+		.references(() => events.id, { onDelete: "cascade" }),
+});
+
+export const attendeesRelations = relations(attendees, ({ one }) => ({
+	event: one(events, {
+		fields: [attendees.eventId],
+		references: [events.id],
+		relationName: "attendee_event",
+	}),
+}));
+
+export type Attendee = InferSelectModel<typeof attendees>;
--- src/database/schemas/event.ts
+++ src/database/schemas/event.ts
@@ -3,7 +3,10 @@ import type { InferSelectModel } from "drizzle-orm";
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const events = sqliteTable("events", {
-	id: text("id").notNull().$defaultFn(() => createId()),
+	id: text("id")
+		.notNull()
+		.primaryKey()
+		.$defaultFn(() => createId()),
 	slug: text("slug").unique().notNull(),
 	title: text("title").notNull(),
 	details: text("details"),
